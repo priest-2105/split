dd47ef0e727d494814183ab9a8166283c32e0fde
priest-2105 <priest2105@gmail.com>
2025-03-06 22:58:52 +0100

    theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import ClientProviders from "@/components/ClientProviders"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Footer } from "@/components/public/layout/Footer"
+import { Navbar } from "@/components/public/layout/Navbar"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,7 +23,11 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <ClientProviders>{children}</ClientProviders>
+          <ClientProviders>
+            <Navbar/>
+            {children}
+            <Footer/>
+            </ClientProviders>
         </ThemeProvider>
       </body>
     </html>
